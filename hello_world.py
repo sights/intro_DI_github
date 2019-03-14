f1b17d7e94520453129af8c9f129acdcb4f56690
sights <thf393@gmail.com>
2019-03-14 18:20:21 +0200

    first commit

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 14 17:55:06 2019
+
+@author: Tali
+"""
+
+print('hello world')
---
+++ hello_you.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 14 17:56:16 2019
+
+@author: Tali
+"""
+
+print("The weather is beautiful today")
